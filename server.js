da48fcc74f28f3c2f6dbe9d69ca48f3592a36885
Mbeng Ngassa <bearphace@gmail.com>
2012-09-28 11:48:38 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+   // route(handle, pathname, response);
+   	request.setEncoding("utf8");
+
+    request.addListener ( "data", function(postDataChunck){
+    	console.log ("Recieved POST data chunk '"+postDataChunck +".");
+    });
+    request.addListener ("end", function () {
+    	route (handle, pathname, response);
+    });
+
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
+}
+
+exports.start = start;
